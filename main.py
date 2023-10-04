6a89b0f61903e4a5b26597dbafb4601ba676bc9b
Paul <pkordana422@gmail.com>
2023-10-04 12:29:15 -0400

    init commit 2

--- main.py
+++ main.py
@@ -27,18 +27,29 @@ MORSE_DICT = {
     'Z': '--..',
 }
 
+def validate_char(letter):
+    """Dictionary lookup to validate input"""
+    return letter.upper() in list(MORSE_DICT.keys())
+
+
+def translate_char(letter):
+    """Dictionary lookup of character input"""
+    return MORSE_DICT[letter.upper()]
 
 # welcome message
 print("This program will take your text input and translate it into morse code.")
-print(list(MORSE_DICT.keys()))
+
 # gather user input
 text_input = input("Enter text to translate: ")
-while text_input not in list(MORSE_DICT.keys()):
-    text_input = input("Enter text to translate: ")
 
 # init output var
 morse_output = ""
 
 # translate each letter
 for c in text_input:
-    pass
+    if validate_char(c):
+        morse_output += translate_char(c)
+    else:
+        morse_output += c
+
+print(morse_output)
